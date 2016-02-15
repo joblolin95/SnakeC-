bef88db61ad7ef294425436fa549f4c7252af2f8
joblolin95 <joblolin@ius.edu>
2016-02-15 15:39:10 -0500

    Changed welcome message text color

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -61,7 +61,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Biondi", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.SystemColors.MenuBar;
+            this.label2.ForeColor = System.Drawing.Color.DeepSkyBlue;
             this.label2.Location = new System.Drawing.Point(484, 70);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(289, 115);
